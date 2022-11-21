359b52805e5934bb43ab4257adbffff5c823e4ed
Rakhesh-NeoITO <rakhesh.j@neoito.com>
2022-11-21 17:21:07 +0530

    task2:Sibling

---
+++ Walking Dom/Task2/task2.js
@@ -0,0 +1,6 @@
+let divDom = document.body.firstElementChild
+let divDom2 = document.body.children[1].children[0].previousSibling
+let divDom3 = document.body.children[1].lastChild.nextSibling
+console.log(divDom)
+console.log(divDom2)//#text
+console.log(divDom3)//null
