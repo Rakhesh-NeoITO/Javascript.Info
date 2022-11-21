1dd25501e565f3fb0f32c9d2f3f61851e6134aaf
Rakhesh-NeoITO <rakhesh.j@neoito.com>
2022-11-21 17:11:14 +0530

    task1:DOM children

---
+++ task1/task1.js
@@ -0,0 +1,6 @@
+let divDom = document.body.firstElementChild
+let divDom2 = document.body.children[1]
+let divDom3 = document.body.children[1].lastElementChild
+console.log(divDom)
+console.log(divDom2)
+console.log(divDom3)
