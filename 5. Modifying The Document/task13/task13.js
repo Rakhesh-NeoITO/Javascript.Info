35520f7ed34575cad7458eb10be1fc7f0d7e46da
Rakhesh-NeoITO <rakhesh.j@neoito.com>
2022-11-22 12:21:10 +0530

    task13:Table remove 'inside <table> tag only contain table related contents'

---
+++ 5. Modifying The Document/task13/task13.js
@@ -0,0 +1,3 @@
+console.log(table); // the table, as it should be
+
+table.remove();// inside <table> tag only supports table related contents and elements only. browser pops out all others from the table 
