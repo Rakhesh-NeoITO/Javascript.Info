2a6d3f3fcfb4a0f6bd1a194415fd98c7a9358e6f
Rakhesh-NeoITO <rakhesh.j@neoito.com>
2022-11-22 11:23:24 +0530

    task10: Make External Links Orange

---
+++ 4. Attributes Properties/task10/task10.js
@@ -0,0 +1,18 @@
+let anchor = document.querySelectorAll('a')
+console.log(anchor)
+
+for(let i = 0; i < anchor.length; i++){
+    // console.log(anchor[i])
+    if(!anchor[i].getAttribute('href')){
+        console.log(anchor[i])
+    }else{
+        if(anchor[i].getAttribute('href').includes('://')){
+            anchor[i].style.color="orange"
+            if(anchor[i].getAttribute('href').startsWith('http://internal.com')){
+                anchor[i].style.color=""
+            }
+        }
+    }
+    
+    
+}
