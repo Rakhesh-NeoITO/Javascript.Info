6ebc66ffde2d1537e48e9b2d1047122b255a84ca
Rakhesh-NeoITO <rakhesh.j@neoito.com>
2022-11-25 11:50:40 +0530

    task38: tree menu

---
+++ 10. Event Delegation/task38/task38.js
@@ -0,0 +1,14 @@
+let tree = document.getElementById('tree')
+console.log(tree)
+
+tree.addEventListener ('click', function (event) {
+  console.log(event.target)
+  if (event.target.tagName = 'li' ){
+    event.target.querySelector('ul').hidden = !event.target.querySelector('ul').hidden;
+    //console.log(event.target)
+    
+    
+}
+
+
+})
