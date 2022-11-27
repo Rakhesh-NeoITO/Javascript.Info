3ca890e32f64c1ab4dd130974fc2a18e3e59a10f
Rakhesh-NeoITO <rakhesh.j@neoito.com>
2022-11-27 11:15:59 +0530

    task40: Tooltip behaviour

---
+++ 10. Event Delegation/task40/task40.js
@@ -0,0 +1,34 @@
+let toolTip;
+
+document.addEventListener('mouseover', function (event) {
+
+  //console.log(event.target.tagName);
+  if (event.target.tagName !== 'BUTTON' ) return
+  
+    console.log(event)
+    toolTip = document.createElement('span');
+    toolTip.className = 'tooltip';
+    toolTip.innerHTML = event.target.innerHTML;
+    document.body.append(toolTip);
+    let coords = event.target.getBoundingClientRect();
+
+    let left = coords.left + (event.target.offsetWidth - toolTip.offsetWidth) / 2;
+      if (left < 0) left = 0;
+
+    let top = coords.top - toolTip.offsetHeight - 5;
+      if (top < 0) {
+        top = coords.top + event.target.offsetHeight + 5;
+      }
+
+      toolTip.style.left = left + 'px';
+      toolTip.style.top = top + 'px';
+  
+})
+document.onmouseout = function(event) {
+
+  if (toolTip) {
+    toolTip.remove();
+    toolTip = null;
+  }
+
+};
