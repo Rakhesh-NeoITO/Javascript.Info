f7bf1f4cf543f56a9c8e2bfc23a370f1a9bd014c
Rakhesh-NeoITO <rakhesh.j@neoito.com>
2022-11-28 12:23:23 +0530

    task47: modified drag and drop

--- 14. Drag and Drop/task47/task47.js
+++ 14. Drag and Drop/task47/task47.js
@@ -3,15 +3,17 @@ let thumb = slider.querySelector('.thumb');
 thumb.onmousedown = function(event) {
   event.preventDefault();
   console.log(event.clientX)
+  console.log(thumb.getBoundingClientRect().left)
 
   let shiftX = event.clientX - thumb.getBoundingClientRect().left;
-
+  let shiftY = event.clientY - thumb.getBoundingClientRect().top;
+  console.log(shiftX)
   document.addEventListener('mousemove', onMouseMove);
   document.addEventListener('mouseup', onMouseUp);
 
   function onMouseMove(event) {
-    let newLeft = event.clientX - shiftX - slider.getBoundingClientRect().left;
-
+    let newLeft = event.clientX - slider.getBoundingClientRect().left - shiftX ;
+    let newTop = event.clientY - slider.getBoundingClientRect().top - shiftY ;
     if (newLeft < 0) {
       newLeft = 0;
     }
@@ -19,8 +21,17 @@ thumb.onmousedown = function(event) {
     if (newLeft > rightEdge) {
       newLeft = rightEdge;
     }
+    if (newTop < 0) {
+      newTop = 0;
+    }
+    let bottomEdge = slider.offsetHeight - thumb.offsetHeight;
+    if (newTop > bottomEdge) {
+      newTop = bottomEdge;
+    }
 
     thumb.style.left = newLeft + 'px';
+    thumb.style.top = newTop + 'px';
+
   }
 
   function onMouseUp() {
