b0f91046248198bceb5455909dec153d3d0be16e
Rakhesh-NeoITO <rakhesh.j@neoito.com>
2022-12-05 09:39:45 +0530

    PART1: task12: is array copied

---
+++ PART 1/4. Arrays/task12/task12.js
@@ -0,0 +1,8 @@
+let fruits = ["Apples", "Pear", "Orange"];
+
+let shoppingCart = fruits;
+shoppingCart.push("Banana");
+
+console.log( fruits.length ); 
+
+//arrays are objects so both have same reference
