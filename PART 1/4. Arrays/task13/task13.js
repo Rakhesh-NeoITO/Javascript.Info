03b082c3b728e6259bffb1247651ba9929d9bd02
Rakhesh-NeoITO <rakhesh.j@neoito.com>
2022-12-05 09:56:52 +0530

    PART1: task13: array operations

---
+++ PART 1/4. Arrays/task13/task13.js
@@ -0,0 +1,12 @@
+let styles = ['Jazz', 'Blues'];
+
+styles.push('Rock-n-Roll');
+styles[styles.length%2 ] = 'Classic';
+console.log(styles[styles.length%2 ])
+
+console.log(styles);
+console.log(styles.shift());
+console.log(styles);
+
+styles.unshift('Rap', 'Reggae');
+console.log(styles);
