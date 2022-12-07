eccdf63516a13dd192475dcc6d6839c6b1f120af
Rakhesh-NeoITO <rakhesh.j@neoito.com>
2022-12-07 11:16:20 +0530

    PART1: task33: teh maximal salary

---
+++ PART 1/7. Destructuring assignment/task33/task33.js
@@ -0,0 +1,22 @@
+let salaries = {
+  "John": 400,
+  "Pete": 300,
+  "Mary": 150
+};
+
+function topSalary(salaries) {
+
+  let maxSalary = 0;
+  let maxName = null;
+
+  for(const [name, salary] of Object.entries(salaries)) {
+    if (maxSalary < salary) {
+      maxSalary = salary;
+      maxName = name;
+    }
+  }
+
+  return maxName + ':' + maxSalary;
+}
+
+console.log( topSalary(salaries) );
